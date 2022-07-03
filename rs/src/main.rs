1c229b8a8b12c9ebaa28147bf31fa1b81971e758
Cooper Lees <me@cooperlees.com>
2022-07-03 14:50:27 -0500

    Update main.rs
    Cleanup some unneeded comments

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use log::info;
 
-/// Search for a pattern in a file and display the lines that contain it.
+/// Clap CLI Args struct
 #[derive(Debug, Parser)]
 struct Cli {
     #[clap(flatten)]
@@ -10,11 +10,8 @@ struct Cli {
 
 use anyhow::Result;
 
-// TODO: BufReader instead of read_to_string()
 fn main() -> Result<()> {
     let args = Cli::parse();
-
-    // Make an env logger with ability to increase our logging
     env_logger::Builder::new()
         .filter_level(args.verbose.log_level_filter())
         .init();
