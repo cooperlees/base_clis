2727f7e6c7c2b580d94979af2e0e889fa0778606
Cooper Ry Lees <me@cooperlees.com>
2022-09-18 16:50:45 -0700

    Add example clap long_about

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -2,9 +2,11 @@ use clap::Parser;
 use clap_verbosity_flag::InfoLevel;
 use log::info;
 
+const LONG_ABOUT: &str = "This is a base CLI to use with new rust projects";
+
 /// Clap CLI Args struct with metadata in help output
 #[derive(Debug, Parser)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about, long_about = LONG_ABOUT)]
 struct Cli {
     #[clap(flatten)]
     verbose: clap_verbosity_flag::Verbosity<InfoLevel>,
