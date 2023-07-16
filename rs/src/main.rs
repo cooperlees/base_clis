8d3894660ab6aa63017f7e65bb77f8c4e574fc58
Cooper Lees <cooper@meta.com>
2023-07-16 16:10:25 -0700

    Move from debug bool to specifying log level from CLI

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
+use clap::ValueEnum;
 use tracing::debug;
 use tracing::info;
 use tracing_glog::Glog;
@@ -11,24 +12,42 @@ use tracing_subscriber::Registry;
 
 const LONG_ABOUT: &str = "This is a base CLI to use with new rust projects";
 
+// This enum can be used to add `log-level` option to CLI binaries.
+#[derive(ValueEnum, Clone, Debug, Copy)]
+pub enum PublicLogLevels {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl From<PublicLogLevels> for LevelFilter {
+    fn from(public_level: PublicLogLevels) -> Self {
+        match public_level {
+            PublicLogLevels::Error => LevelFilter::ERROR,
+            PublicLogLevels::Warn => LevelFilter::WARN,
+            PublicLogLevels::Info => LevelFilter::INFO,
+            PublicLogLevels::Debug => LevelFilter::DEBUG,
+            PublicLogLevels::Trace => LevelFilter::TRACE,
+        }
+    }
+}
+
 /// Clap CLI Args struct with metadata in help output
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = LONG_ABOUT)]
 struct Cli {
-    /// Enable debug logging
-    #[clap(short, long)]
-    debug: bool,
-
     /// Number that brings you luck
     #[clap(short, long, value_parser, default_value_t = 69)]
-    lucky_number: u8,
+    a_lucky_number: u8,
+
+    /// Adjust the console log-level
+    #[arg(long, short, value_enum, ignore_case = true, default_value = "Info")]
+    log_level: PublicLogLevels,
 }
 
-fn setup_logging(debug: bool) {
-    let log_filter_level = match debug {
-        true => LevelFilter::DEBUG,
-        false => LevelFilter::INFO,
-    };
+fn setup_logging(log_filter_level: LevelFilter) {
     let fmt = fmt::Layer::default()
         .with_writer(std::io::stderr)
         .event_format(Glog::default().with_timer(tracing_glog::LocalTime::default()))
@@ -42,7 +61,7 @@ fn setup_logging(debug: bool) {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    setup_logging(args.debug);
+    setup_logging(args.log_level.into());
 
     info!("Cooper's amazing base CLI. Args: {:?}", args);
     debug!("Debug logging enabled");
